3ebbb48121fd05da742593e63be378b47ba8828f
Ali <ali.baqeri.yazdabady@gmail.com>
2022-07-15 17:30:43 +0430

    1401-04-23, p7

--- lib/src/dbWeb.dart
+++ lib/src/dbWeb.dart
@@ -7,16 +7,16 @@ Future<String> openDoc(String path) async {
     window.indexedDB!.open(path, version: 1)
   }*/
 
-  if (window.localStorage.containsKey(path)){
+  /*if (window.localStorage.containsKey(path)){
     return window.localStorage[path]!;
-  }
+  }*/
 
   print('☼☼☼☼☼☼ IsisDB [web-openDoc] C');
   return '';
 }
 
 Future<bool> writeDoc(String path, String data, bool backup){
-  if (window.localStorage.containsKey(path)){
+  /*if (window.localStorage.containsKey(path)){
     if(backup){
       final oldBk = '$path.bk';
 
@@ -24,17 +24,17 @@ Future<bool> writeDoc(String path, String data, bool backup){
         window.localStorage.remove(oldBk);
         window.localStorage[oldBk] = window.localStorage[path]!;
       }
-      catch (e){/**/}
+      catch (e){*//**//*}
     }
   }
 
-  window.localStorage[path] = data;
+  window.localStorage[path] = data;*/
 
   return Future.value(true);
 }
 
 Future<bool> appendDoc(String path, String data, bool backup){
-  if (window.localStorage.containsKey(path)){
+  /*if (window.localStorage.containsKey(path)){
     if(backup){
       final oldBk = '$path.bk';
 
@@ -42,22 +42,22 @@ Future<bool> appendDoc(String path, String data, bool backup){
         window.localStorage.remove(oldBk);
         window.localStorage[oldBk] = window.localStorage[path]!;
       }
-      catch (e){/**/}
+      catch (e){*//**//*}
     }
 
     window.localStorage[path] = window.localStorage[path]! + data;
   }
   else {
     window.localStorage[path] = data;
-  }
+  }*/
 
   return Future.value(true);
 }
 
 Future<String> deleteDoc(String path) async {
-  if (window.localStorage.containsKey(path)){
+  /*if (window.localStorage.containsKey(path)){
     window.localStorage.remove(path);
-  }
+  }*/
 
   return path;
 }
